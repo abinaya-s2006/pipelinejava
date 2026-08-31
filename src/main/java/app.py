ca7895fb80b30f69d9ba6bc9d070186dd6e11657
abinaya-s2006 <abinaya232803@gmail.com>
2026-08-31 13:37:40 +0530

    Refactor HelloWorld class to app and update message

--- src/main/java/app.py
+++ src/main/java/app.py
@@ -1,5 +1,7 @@
-public class HelloWorld {
+package com.example;
+
+public class app {
     public static void main(String[] args) {
-        System.out.println("Hello World from CI/CD Pipeline!");
+        System.out.println("Hello, World!");
     }
 }
